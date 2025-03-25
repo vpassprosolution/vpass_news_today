39dd27182fa0fad827b1006674601aac64470cd7
vpasspro <vpassprosolution@gmail.com>
2025-03-25 20:46:20 +0800

    Auto-create table on app startup

--- news_today.py
+++ news_today.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+import init_db
 
 app = FastAPI()
+
+# 🧠 Auto-create the table at startup
+init_db.create_table()
 
 @app.get("/get_today_news")
 def get_today_news():
