80605555586c04dd5b97ab685a3037da78ec48da
vpasspro <vpassprosolution@gmail.com>
2025-03-25 20:43:19 +0800

    fix

--- init_db.py
+++ init_db.py
@@ -1,13 +1,14 @@
 import psycopg2
+import os
 
 def create_table():
     try:
         conn = psycopg2.connect(
-            dbname="railway",
-            user="postgres",
-            password="vVMyqWjrqgVhEnwyFifTQxkDtPjQutGb",
-            host="interchange.proxy.rlwy.net",
-            port="30451"
+            dbname=os.getenv("POSTGRES_DB"),
+            user=os.getenv("POSTGRES_USER"),
+            password=os.getenv("POSTGRES_PASSWORD"),
+            host=os.getenv("POSTGRES_HOST"),
+            port=os.getenv("POSTGRES_PORT")
         )
 
         cursor = conn.cursor()
